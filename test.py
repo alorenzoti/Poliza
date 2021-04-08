6ce71c89a611b7cf8e6730eb2d7be99826e9d49e
Anon <anonymous.user@mailservice.com>
2021-04-09 00:57:05 +0200

    true

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import arcpy
 ruta_precipitaciones = r"C:\script\climatologia\precipitaciones.gdb"
 arcpy.env.workspace = ruta_precipitaciones
 mes = "febrero"
+debug = True
 
 if debug:
     print("estoy copiando precipitaciones {mes} ".format(mes=mes))
